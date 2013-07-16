58ff7a66f44ac00b856c54b0c4205a44c80741d4
ErickChemevski <ecespindula@inf.ufpel.edu.br>
2013-07-16 20:31:12 -0300

    Update trab.py
    Adicionando recursividade

--- codigos/trab.py
+++ codigos/trab.py
@@ -1,32 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
- 
- 
 import urllib.request
+
+def recur(prof,url):
+ if prof != 0 :
+		resposta = urllib.request.urlopen(url)
  
-url = "http://heeeeeeeey.com"
-resposta = urllib.request.urlopen(url)
+		line = resposta.readline().decode("utf-8")
  
-line = resposta.readline().decode("utf-8")
- 
-#line = 'Made by <a href="http://bod.ge">Mike Bodge</a> <a href="http://itunes.apple.com/us/album/greatest-hits-naughtys-nicest/id50235454?uo=4">iTunes</a>'
-while line != '' :
-       
-        if '<a' in line :
-                index = line.find('<a')
-                line = line[index:]
-                if '"' in line :
-                        index = line.find('"')
-                        line = line[index + 1:]
-                        if '"' in line :
-                                index = line.find('"')
-                                link = line[:index]
-                                line = line[index:]
-                                print(link)
+		#line = 'Made by <a href="http://bod.ge">Mike Bodge</a> <a href="http://itunes.apple.com/us/album/greatest-hits-naughtys-nicest/id50235454?uo=4">iTunes</a>'
+		while line != '' :
+			
+				if '<a' in line :
+						index = line.find('<a')
+						line = line[index:]
+						if '"' in line :
+								index = line.find('"')
+								line = line[index + 1:]
+								if '"' in line :
+										index = line.find('"')
+										link = line[:index]
+										line = line[index:]
+										print(link)
+										if prof > 0:
+											recur(prof-1,link)
                                
-                        else :
-                                line = resposta.readline().decode("utf-8")
-                else :
-                        line = resposta.readline().decode("utf-8")     
-        else :                 
-                line = resposta.readline().decode("utf-8")
+								else :
+										line = resposta.readline().decode("utf-8")
+						else :
+								line = resposta.readline().decode("utf-8")     
+				else :                 
+						line = resposta.readline().decode("utf-8")
+
+prof = 2
+url = http://www.baixeturbo.org/
+recur(prof,url)
