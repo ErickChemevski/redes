2e8ef26042e44529e0114c6edb3756340faabd84
lisandroluizdasilva <lisandrolds@gmail.com>
2013-07-16 20:23:53 -0300

    Update trab.py
    Buscando Links na URL

--- codigos/trab.py
+++ codigos/trab.py
@@ -1,5 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+ 
+ 
 import urllib.request
-
+ 
 url = "http://heeeeeeeey.com"
 resposta = urllib.request.urlopen(url)
-print (resposta.read())
+ 
+line = resposta.readline().decode("utf-8")
+ 
+#line = 'Made by <a href="http://bod.ge">Mike Bodge</a> <a href="http://itunes.apple.com/us/album/greatest-hits-naughtys-nicest/id50235454?uo=4">iTunes</a>'
+while line != '' :
+       
+        if '<a' in line :
+                index = line.find('<a')
+                line = line[index:]
+                if '"' in line :
+                        index = line.find('"')
+                        line = line[index + 1:]
+                        if '"' in line :
+                                index = line.find('"')
+                                link = line[:index]
+                                line = line[index:]
+                                print(link)
+                               
+                        else :
+                                line = resposta.readline().decode("utf-8")
+                else :
+                        line = resposta.readline().decode("utf-8")     
+        else :                 
+                line = resposta.readline().decode("utf-8")
